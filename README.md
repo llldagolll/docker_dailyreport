3d3c9714442da3c2e056214cdf1e11845e5ea1e0
nobuyaaa <anobu4@gmail.com>
2021-10-09 17:41:37 +0900

    header完成 sidebarつくる

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ https://gurutaka-log.com/react-input-usestate-useref
 
 - TailwindCSSの導入の仕方
 https://qiita.com/Annoske/items/1087a1205d3783ea3d94
+https://shuu1104.com/2021/04/2827/
 
 - React-router-domの導入の仕方
 npm install react-router-dom
--- app/front/src/components/Header.js
+++ app/front/src/components/Header.js
@@ -2,28 +2,28 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "../tailwind.css";
 
-const Layout = () => {
+const Header = () => {
     return (
-        <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
-                <nav className="bg-green-800 w-screen">
-                    <div className="flex items-center pl-8 h-14">
-                        <div className="flex space-x-4">
-                            <Link to={`/home`}>
-                                <a className="text-green-300 hover:bg-green-700 px-3 py-2 rounded">Home</a>
-                            </Link>
-                            <Link to={`/home`}>
-                                <a className="text-green-300 hover:bg-green-700 px-3 py-2 rounded">ブログ</a>
-                            </Link>
-                            <Link to={`/home`}>
-                                <a className="text-green-300 hover:bg-green-700 px-3 py-2 rounded">問い合わせ</a>
-                            </Link>
-                        </div>
-                    </div>
-                </nav>
-        </div>
+            <nav className="items-center justify-between flex bg-gray-800 bg-opacity-90 px-12 py-4 my-4 mx-auto shadow-2xl w-screen">
+                <div>
+                     <button>
+                        <svg className="h-6 w-6 fill-current text-white" viewBox="0 0 24 24">
+                            <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+                        </svg>
+                    </button>
+                </div>
+                <div>
+                    <p className="text-2xl text-white font-semibold">WithUs</p>
+                </div>
+                <div className="text-2xl text-white font-semibold inline-flex items-center">
+                    <Link to={`/home`}>
+                        <a className="ml-5 px-2 py-1">Logout</a>
+                    </Link>
+                </div>
+            </nav>
     )
 }
 
 
 
-export default Layout;
+export default Header;
