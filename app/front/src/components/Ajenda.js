bb98c6d2ee4495423a94629798a26cb23f33c2b5
nobuyaaa <anobu4@gmail.com>
2021-10-11 14:01:41 +0900

    ボタンクリック時に関数の実行確認

--- app/front/src/components/Ajenda.js
+++ app/front/src/components/Ajenda.js
@@ -3,48 +3,29 @@ import '../tailwind.css';
 
 
 const Ajenda = () =>  {
-  const textRef = useRef(null);
-  // const  onclick = () => {
-  //   console.log(textRef)
-  // }
 
-  return (
-    <div className="flex flex-col h-screen">
-        <div className="flex-auto">
-            <div className="flex justify-center mt-20">
-                <div className="w-9/12 border bg-white">
-                    <div className="my-16 text-center">
-                        <input ref={textRef} type="text" />
-                        <button class="w-24 m-4 p-4 rounded-sm bg-red-500 text-center text-white" type="button" onClick={()=> alert(textRef.current.value)}>値の確認</button>
+    const textRef = useRef(null);
+
+    const handleClick = () => {
+        alert(textRef.current.value);
+    }
+
+      return (
+        <div className="flex flex-col h-screen">
+            <div className="flex-auto">
+                <div className="flex justify-center mt-20">
+                    <div className="w-9/12 border bg-white">
+                        <div className="my-16 text-center">
+                            <input ref={textRef} type="text" />
+
+                            <button class="w-24 m-4 p-4 rounded-sm bg-red-500 text-center text-white" type="button" onClick={() => handleClick()}>値の送信</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+      );
 }
-
-
-
-
-
-// const Ajenda = () =>  {
-//   return (
-//     <Grid container alignItems="center" justify="center">
-//       <Box
-//         component="form"
-//         sx={{
-//           '& > :not(style)': { m: 1, width: '25ch' },
-//         }}
-//         noValidate
-//         autoComplete="off"
-//       >
-//         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-//       </Box>
-//     </Grid>
-//   );
-// }
-
 
 
 export default Ajenda
